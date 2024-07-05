8ae7c82a7c2411fcbdf91c00bf4e34e937a67158
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2024-07-05 08:27:01 +0000

    log PII data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -43,10 +43,12 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """initialize instance"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """format logging line"""
         message = super(RedactingFormatter, self).format(record)
         return filter_datum(self.fields, self.REDACTION, message,
                             self.SEPARATOR)

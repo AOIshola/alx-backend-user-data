07f8c3167213354b319b4eca890654d57676f1d1
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2024-07-09 18:41:44 +0000

    implement basic auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -15,24 +15,15 @@ class Auth:
         if path is None or excluded_paths is None or len(excluded_paths) == 0:
             return True
 
-        # Normalize path
         if path[-1] != '/':
             path += '/'
 
         for excluded_path in excluded_paths:
-            # Normalize excluded_path
-            if excluded_path[-1] != '/':
-                excluded_path += '/'
-                print(excluded_path)
-
-            # Check for wildcard match
             if excluded_path.endswith('*'):
-                print(excluded_path)
                 if path.startswith(excluded_path[:-1]):
                     return False
             elif path == excluded_path:
                 return False
-
         return True
 
     def authorization_header(self, request=None) -> str:

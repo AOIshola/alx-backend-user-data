b70e1f6a61324d82ddf955d50a17aafa41171700
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2024-07-09 16:53:13 +0000

    implement basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""Auth Module"""
+
+from flask import request
+import re
+from typing import List, TypeVar
+
+
+class Auth:
+    """ Auth class
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ defines auth requirement
+        """
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        # Normalize path
+        if path[-1] != '/':
+            path += '/'
+
+        for excluded_path in excluded_paths:
+            # Normalize excluded_path
+            if excluded_path[-1] != '/':
+                excluded_path += '/'
+                print(excluded_path)
+
+            # Check for wildcard match
+            if excluded_path.endswith('*'):
+                print(excluded_path)
+                if path.startswith(excluded_path[:-1]):
+                    return False
+            elif path == excluded_path:
+                return False
+
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """ auth header
+        """
+        if request is None or request.headers.get('Authorization') is None:
+            return None
+        return request.headers.get('Authorization')
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ current authenticated user
+        """
+        return None

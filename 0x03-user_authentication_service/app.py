3eb3951b6a1b21a69d2d5ea492742901113ae6d5
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2024-07-19 08:10:38 +0000

    implement user auth in flask

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -51,9 +51,10 @@ def login():
 def logout():
     """ logs a user out and destroys the session
     """
+    session_id = request.cookies.get('session_id')
+    if not session_id:
+        abort(403)
     try:
-        session_id = request.cookies.get('session_id')
-        AUTH.get_user_from_session_id(session_id)
         user = AUTH._db.find_user_by(session_id=session_id)
         AUTH.destroy_session(user.id)
         return redirect('/')
